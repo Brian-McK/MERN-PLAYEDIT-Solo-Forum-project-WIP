09a2381dd5d5d599981d43725278a4d1eb6e9dd5
Brian McKenna <D00197352@student.dkit.ie>
2021-08-10 11:15:16 +0100

    Start work on header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Header MainText={"This is my header"}></Header>
+    </div>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+
+function Header(props) {
+  return (
+    <div className="Header">
+        <h1>{props.MainText}</h1>
+    </div>
+  );
+}
+
+export default Header;
