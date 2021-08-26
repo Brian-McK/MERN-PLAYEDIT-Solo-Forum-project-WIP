0c08d6ac5a759961c198d0b7ba01c85c942e2f8e
Brian McKenna <D00197352@student.dkit.ie>
2021-08-26 12:54:21 +0100

    Post image preview done

--- src/Components/MainSection/HomePost.js
+++ src/Components/MainSection/HomePost.js
@@ -1,6 +1,9 @@
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import PostVote from "./PostVote";
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles(() => ({
   customWidthContainer: {
@@ -10,9 +13,16 @@ const useStyles = makeStyles(() => ({
     border: "2px solid green",
     minHeight: "inherit",
   },
+  postPreviewImage: {
+    minHeight: "120px",
+    maxHeight: "120px",
+  },
+  media: {
+    height: "120px",
+  },
 }));
 
-function HomePost() {
+function HomePost(props) {
   const classes = useStyles();
 
   return (
@@ -35,23 +45,50 @@ function HomePost() {
         </Grid>
         <Grid
           item
+          container
+          direction="row"
+          alignItems="center"
           className={classes.customGridItemIcons}
           xs={7}
           sm={6}
           md={7}
           lg={7}
         >
-          Main Section
+          <Grid item xs={12}>
+            Main section 1
+          </Grid>
+          <Grid item xs={12}>
+            Main Section 2
+          </Grid>
+          <Grid item xs={12}>
+            Main section 3
+          </Grid>
+          <Grid item xs={12}>
+            Main section 4
+          </Grid>
         </Grid>
         <Grid
           item
+          container
+          alignContent={"center"}
           className={classes.customGridItemIcons}
           xs={3}
           sm={4}
           md={3}
           lg={4}
         >
-          Image Preview
+          <Grid item xs={12} className={classes.postPreviewImage}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  className={classes.media}
+                  image={props.Image}
+                  title={props.Title}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
         </Grid>
       </Grid>
     </div>
--- src/Components/MainSection/HomePosts.js
+++ src/Components/MainSection/HomePosts.js
@@ -1,6 +1,8 @@
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import HomePost from "./HomePost";
+import Image from "../../images/gaming.jpg";
+import Image2 from "../../images/ps4.png";
 
 const useStyles = makeStyles(() => ({
   customWidthContainer: {
@@ -20,16 +22,22 @@ function HomePosts() {
         spacing={1}
       >
         <Grid item xs={12}>
-          <HomePost></HomePost>
+          <HomePost Image={Image} Title={"Some image title"}></HomePost>
         </Grid>
         <Grid item xs={12}>
-          <HomePost></HomePost>
+          <HomePost Image={Image2} Title={"Some image title"}></HomePost>
         </Grid>
         <Grid item xs={12}>
-          <HomePost></HomePost>
+          <HomePost Image={Image} Title={"Some image title"}></HomePost>
         </Grid>
         <Grid item xs={12}>
-          <HomePost></HomePost>
+          <HomePost Image={Image2} Title={"Some image title"}></HomePost>
+        </Grid>
+        <Grid item xs={12}>
+          <HomePost Image={Image} Title={"Some image title"}></HomePost>
+        </Grid>
+        <Grid item xs={12}>
+          <HomePost Image={Image2} Title={"Some image title"}></HomePost>
         </Grid>
       </Grid>
     </div>
